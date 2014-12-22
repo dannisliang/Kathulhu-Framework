ead391bcd2305bf9102d30e106f9f6ac6b75bbcc
Mathieu Chénard <math.chenard@gmail.com>
2014-12-21 23:02:07 -0500

    Pooling
    work in progress, probably some bugs to find and fix

---
+++ Pooling/ObjectPool.cs
@@ -0,0 +1,119 @@
+﻿namespace Kathulhu
+{
+    using UnityEngine;
+    using System.Collections.Generic;
+
+
+    /// <summary>
+    /// An object pool for a distinct Prefab.
+    /// </summary>
+    [System.Serializable]
+    public class ObjectPool
+    {
+
+        public bool IsInitialized { get; private set; }
+
+
+        /// <summary>
+        /// Reference to the prefab used by this object pool.
+        /// </summary>
+        public GameObject Prefab;
+
+        /// <summary>
+        /// How many instance of the prefab should be instantiated when this object pool is initialized.
+        /// </summary>
+        public int amountOfObjectsToPreload;
+
+        /// <summary>
+        /// The Transform pool objects should be parented to.
+        /// </summary>
+        public Transform ParentTransform;
+
+
+        /// <summary>
+        /// The list of available objects.
+        /// </summary>
+        private List<GameObject> _pooledObjects;
+
+
+
+        /// <summary>
+        /// Setup the object pool and preload required instances of the prefab.
+        /// </summary>
+        /// <param name="parent">The transform to use as the parent of pooled objects</param>
+        public void Initialize( GameObject prefab, Transform parent )
+        {
+            Prefab = prefab;
+            ParentTransform = parent;
+
+            _pooledObjects = new List<GameObject>();
+
+            for ( int x = 0; x < amountOfObjectsToPreload; x++ )
+                CreatePooledInstance();
+
+            IsInitialized = true;
+        }
+
+        /// <summary>
+        /// Creates an instance of the prefab and adds it to the pool.
+        /// </summary>
+        public void CreatePooledInstance()
+        {
+            GameObject poolObject = Object.Instantiate( Prefab ) as GameObject;
+            poolObject.name = Prefab.name;
+
+            _pooledObjects.Add( poolObject );
+
+            Deactivate( poolObject );
+        }
+
+
+        /// <summary>
+        /// Deactivates a GameObject and returns it to the pool.
+        /// </summary>
+        /// <param name="poolObject">An instance of this pool's prefab that we want to deactivate</param>
+        public void Deactivate( GameObject poolObject )
+        {
+            if ( poolObject == null )
+                return;
+
+            poolObject.transform.parent = ParentTransform;
+            poolObject.SetActive( false );
+        }
+
+
+        /// <summary>
+        /// Spawn an object from the pool at the specified position and rotation.
+        /// </summary>
+        public GameObject Spawn( Vector3 pos, Quaternion rot )
+        {
+            if ( _pooledObjects == null )
+                return null;
+
+            if ( _pooledObjects.Count > 0 )
+            {
+                GameObject spawnedObject = _pooledObjects[0];
+                _pooledObjects.RemoveAt( 0 );
+
+                if ( spawnedObject == null )
+                {
+                    Debug.LogError("Object pool has a null reference of prefab " + Prefab.name );
+                    return null;
+                }
+
+                spawnedObject.transform.position = pos;
+                spawnedObject.transform.rotation = rot;
+                spawnedObject.SetActive( true );
+
+                return spawnedObject;
+            }
+            else
+            {
+                CreatePooledInstance();
+                return Spawn( pos, rot );
+            }
+
+        }
+
+    }
+}
---
+++ Pooling/PoolsManager.cs
@@ -0,0 +1,106 @@
+﻿namespace Kathulhu
+{
+    using UnityEngine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Global pooling manager. Holds, initializes and manages multiple object pools.
+    /// </summary>
+    public class PoolsManager : MonoBehaviour
+    {
+
+        public static PoolsManager Instance { get; private set; }
+
+        /// <summary>
+        /// The list of object pools. Each pool manages a single prefab's instances.
+        /// </summary>
+        public List<ObjectPool> ObjectPools;
+
+
+        /// <summary>
+        /// Returns a pool that handles the specified prefab
+        /// </summary>
+        /// <param name="prefabName">The name of the prefab we're fetching the pool for</param>
+        public ObjectPool GetObjectPool( string prefabName )
+        {
+            return ObjectPools.FirstOrDefault( x => x.Prefab.name == prefabName );
+        }
+
+        /// <summary>
+        /// Adds an object pool for the specified prefab if it doesn't exist already
+        /// </summary>
+        /// <param name="prefab">The prefab we want to pool</param>
+        public void Add( GameObject prefab )
+        {
+            if ( prefab == null )
+                throw new MissingReferenceException( "Cannot create an object pool from a null reference." );
+
+            ObjectPool pool = GetObjectPool( prefab.name );
+            if ( pool == null )
+            {
+                pool = new ObjectPool();
+                pool.Initialize( prefab, transform );
+                ObjectPools.Add( pool );
+            }
+        }
+
+        /// <summary>
+        /// Deactivates a gameObject and returns it to it's pool. If no pool is found for this gameObject, it is destroyed.
+        /// </summary>
+        public void Deactivate( GameObject gameObject )
+        {
+            ObjectPool pool = GetObjectPool( gameObject.name );
+            if ( pool != null )
+            {
+                pool.Deactivate( gameObject );
+            }
+            else
+            {
+                gameObject.SetActive(false);
+                Destroy(gameObject);
+            }
+        }
+
+
+        /// <summary>
+        /// Spawns an instance of the pooled gameobject.
+        /// </summary>
+        public GameObject Spawn( string prefabName )
+        {
+            return Spawn( prefabName, Vector3.zero, Quaternion.identity );
+        }
+
+        /// <summary>
+        /// Spawns an instance of the pooled gameobject at the specified position and rotation.
+        /// </summary>
+        public GameObject Spawn( string prefabName, Vector3 pos, Quaternion rot )
+        {
+            ObjectPool pool = GetObjectPool( gameObject.name );
+            if ( pool != null )
+                return pool.Spawn( Vector3.zero, Quaternion.identity );
+
+            return null;
+        }
+
+
+        void Awake()
+        {
+            Instance = this;
+
+            if ( ObjectPools == null )
+                ObjectPools = new List<ObjectPool>();
+
+            for ( int i = 0; i < ObjectPools.Count; i++ )
+            {
+                if ( ObjectPools[ i ].Prefab == null )
+                    continue;
+
+                if ( !ObjectPools[ i ].IsInitialized )
+                    ObjectPools[ i ].Initialize( ObjectPools[ i ].Prefab, transform );
+            }
+        }
+
+    }
+}
